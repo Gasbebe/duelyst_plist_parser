789f4b44fef9686d281cae71f37259f929a9093c
이황희 <lhh@192.168.nate.com>
2023-07-16 13:44:13 +0900

    corrected main

--- main.py
+++ main.py
@@ -4,8 +4,13 @@ import glob
 from PIL import Image
 
 def print_info(path):
+    
     parse = d_parse()
     parse.load(os.path.join(os.getcwd(), path))
+
+    directory_path = os.path.dirname(path)
+    out_put_dir_path = "output" + os.sep + directory_path
+    os.makedirs(out_put_dir_path, exist_ok=True)
 
     image_path = path.replace("plist", "png")
     if len(parse.get_frames()) > 0 :
@@ -29,7 +34,7 @@ def print_info(path):
             # 잘린 이미지가 여러개 있으면 저장
             if len(cropped_image_list) > 1 :
                 # cropped_image_list[0].save("output/" + parse.get_file_name().replace("png", "gif"), save_all=True, append_images=cropped_image_list[1:], optimize=False, duration=10, loop=1, transparency=0, disposal = 2)
-                cropped_image_list[0].save("output/" + frame_type + ".gif", save_all=True, append_images=cropped_image_list[1:], optimize=False, duration=10, loop=1, transparency=0, disposal = 2)
+                cropped_image_list[0].save(out_put_dir_path  + os.sep + frame_type + ".gif", save_all=True, append_images=cropped_image_list[1:], optimize=False, duration=10, loop=1, transparency=0, disposal = 2)
                 cropped_image_list.clear()
 
             # if len(cropped_image_list) > 1 :
@@ -52,6 +57,11 @@ def print_info(path):
 
         one_frame = image.crop((x1,y1, x2,y2))
         cropped_image_list.append(one_frame)
+        
+    #마지막 이미지 
+    if len(cropped_image_list) > 1 :
+        cropped_image_list[0].save(out_put_dir_path  + os.sep + frame_type + ".gif", save_all=True, append_images=cropped_image_list[1:], optimize=False, duration=10, loop=1, transparency=0, disposal = 2)
+        cropped_image_list.clear()
 
 def main():
     print("Hello, World!")
